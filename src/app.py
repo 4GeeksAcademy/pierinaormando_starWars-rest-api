06b82374ce33a15dc760d5dbd8c7c631eeb24638
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-12-16 00:01:55 +0000

    crud planets,basic endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,82 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#get all user
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_user():
+    all_users = User.query.all()
+    serialized_all_users = list(map(lambda user: user.serialize(), all_users))
+    return jsonify({'msg': 'Los usuarios obtenidos son:', 'results': serialized_all_users}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#get one user
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    print(user_id)
+    user = User.query.get(user_id)
+    serialized_user = user.serialize()
+    return jsonify({'msg': 'El usuario obtenido es:', 'results': serialized_user}), 200
 
-    return jsonify(response_body), 200
+#get all planets
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    all_planets = Planet.query.all()
+    serialized_all_planets = list(map(lambda planet: planet.serialize(), all_planets))
+    return jsonify({'msg': 'Planetas obtenidos:', 'results': serialized_all_planets}), 200
+
+#get one planet
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    print(planet_id)
+    planet = Planet.query.get(planet_id)
+    serialized_planet = planet.serialize()
+    return jsonify({'msg': 'El planeta obtenido es:', 'results': serialized_planet}), 200
+
+#add planet
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar información en el body'}), 400
+    if "name" not in body:
+        return jsonify({'msg': 'El campo name es obligatorio'})
+    if "diameter" not in body:
+        return jsonify({'msg': 'El campo diameter es obligatorio'})
+    if "climate" not in body:
+        return jsonify({'msg': 'El campo climate es obligatorio'})
+    new_planet = Planet()
+    new_planet.name = body['name']
+    new_planet.diameter = body['diameter']
+    new_planet.climate = body['climate']
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify({'msg': 'Planeta creado con éxito'})
+    
+#edit planet name
+@app.route('/planets/<int:planet_id>', methods=['PUT'])
+def update_planet(planet_id):
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar información en el body'}), 400
+    if "name" not in body:
+        return jsonify({'msg': 'El campo name es obligatorio'})
+    """ if "diameter" not in body:
+        return jsonify({'msg': 'El campo diameter es obligatorio'})
+    if "climate" not in body:
+        return jsonify({'msg': 'El campo climate es obligatorio'}) """
+    planet = Planet.query.get(planet_id)
+    planet.name = body['name']
+    """ planet.diameter = body['diameter']
+    planet.climate = body['climate'] """
+    db.session.commit()
+    return jsonify({'msg': 'El nombre del planeta se actualizó con éxito'})
+
+#delete planet
+@app.route('/planets/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify({'msg': 'El planeta se eliminó con éxito'})
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,43 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__:'user'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+#informacion cuando se hace print
     def __repr__(self):
-        return '<User %r>' % self.username
+        return 'Usuario con id {} y nombre {}'.format(self.id, self.nombre)
 
+#convertir de clase a objeto
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    __tablename__:'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    climate = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
+
+#informacion cuando se hace print
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+#convertir de clase a objeto
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "diameter": self.diameter
         }
